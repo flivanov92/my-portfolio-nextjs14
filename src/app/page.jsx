45140f4f1b58391009e9de4741722d1bb7e77c56
Alex <flivanov92@gmail.com>
2023-11-03 12:34:06 +0000

    add  nextjs14, styled components, turbopack,  deploy on vercel  #1

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hi, I am Alex Ivanov</h1>
+    </div>
+  );
+};
+
+export default Home;
