75b8d0dde4559f0266710801cf5ee73a1856633e
Alex <flivanov92@gmail.com>
2023-12-09 13:59:29 +0000

    feat: add speed insight

--- src/ui/atom/hover/hover.style.js
+++ src/ui/atom/hover/hover.style.js
@@ -1,18 +1,17 @@
 'use client';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  background-color: red;
 `;
 
 export const Card = styled.div`
   height: 500px;
   flex: 1;
   transition: 0.5s;
-  background-color: yellow;
   background-image: url('/image/cullinan.jpeg');
   background-size: cover;
   background-position: center;
@@ -22,19 +21,16 @@ export const Card = styled.div`
   }
 
   &:nth-child(2) {
-    background-color: green;
     background-image: url('/image/911.jpeg');
     background-size: cover;
     background-position: center;
   }
   &:nth-child(3) {
-    background-color: orange;
     background-image: url('/image/rs5.jpeg');
     background-size: cover;
     background-position: center;
   }
   &:nth-child(4) {
-    background-color: purple;
     background-image: url('/image/g.jpeg');
     background-size: cover;
     background-position: center;
